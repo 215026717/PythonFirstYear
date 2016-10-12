1b399e70389a829fe8bfe6c0f220804214aa1d41
Mongezi <mongezi.nene@gmail.com>
2016-10-12 22:23:08 +0200

    Add files via upload

--- Clock.py
+++ Clock.py
@@ -1,79 +1,57 @@
 import turtle
 import datetime
+play = False
 wn = turtle.Screen()
+wn.delay(0.5)
 clok= turtle.Turtle()
-clok.ht()
-wn.register_shape("sec", ((0,0),(0,100), (-5,100), (0,105), (5,100),(0,100)))
-wn.register_shape("min", ((0,0),(0,60), (-3,60), (0,63), (3,60),(0,60)))
-wn.register_shape('hour', ((0,0),(0,30), (-3,30), (0,33), (3,30),(0,30)))
 sc = turtle.Turtle()
 mn = turtle.Turtle()
 hr = turtle.Turtle()
-sc.ht()
-mn.ht()
-hr.ht()
+dot = turtle.Turtle()
+clok.ht()
+wn.register_shape("sec", ((0,-15),(-0.5,5),(0,170),(0.5,5),(0,-15)))
+wn.register_shape("min", ((0,0),(-1.5,5),(0,170),(1.5,5),(0,0)))
+wn.register_shape('hour', ((0,0),(-1.5,5),(0,100),(1.5,5),(0,0)))
+dot.shape('circle')
+dot.shapesize(0.2)
+
 def time():
-    wn.title('{0}:{1}:{2}'.format(datetime.datetime.now().hour,datetime.datetime.now().minute,datetime.datetime.now().second))
+    if play:        
+        wn.title('{0}:{1}:{2}'.format(datetime.datetime.now().hour,datetime.datetime.now().minute,datetime.datetime.now().second))
+        sc.seth((-(int(str(datetime.datetime.now().second))))*6+90)
+        mn.seth((-(6*datetime.datetime.now().minute+(datetime.datetime.now().second/10)))+90)
+        hr.seth((-(30*(datetime.datetime.now().hour+(datetime.datetime.now().minute/60)))+90))
+     
     wn.ontimer(time,1000)
     
-def hands():
-
+def switch(x,y):
+    global play
+    play = not play
+    time()
     
-    sc.shape('sec')
-    mn.shape('min')
-    hr.shape('hour')
-    
-    sc.penup()
-    mn.penup()
-    hr.pu()
-    
-    
-    sc.left(90)
-    mn.left(90)
-    hr.left(90)
-    sc.rt(6*datetime.datetime.now().second)
-    mn.rt(6*datetime.datetime.now().minute)
-    hr.rt(30*datetime.datetime.now().hour)
-    
-    sc.st()
-    mn.st()
-    hr.st()
-
-def sec_movement():
-    sc.rt(6)
-    wn.ontimer(sec_movement,1000)
-
-def min_movement():
-    mn.rt(6)
-    wn.ontimer(min_movement,(60000))
-def hr_movement():
-    hr.rt(6)
-    wn.ontimer(hr_movement,720000)
-
-def clock():
-    clok.ht()
-    clok.pu()
-    clok.right(90)
-    clok.fd(200)
-    clok.left(90)
+clok.pu()
+clok.right(90)
+clok.fd(200)
+clok.left(90)
+clok.pd()
+clok.circle(200)
+clok.pu()
+clok.home()
+for i in range(12):
+    clok.fd(150)
     clok.pd()
-    clok.circle(200)
+    clok.fd(30)
     clok.pu()
-    clok.home()
-    for i in range(12):
-        clok.fd(150)
-        clok.pd()
-        clok.fd(30)
-        clok.pu()
-        clok.fd(20)
-        clok.bk(200)
-        clok.right(360/12)
-   
-clock()    
-hands()
-sec_movement()
-min_movement()
-hr_movement()
-time()
+    clok.bk(180)
+    clok.rt(360/12)
 
+sc.shape('sec')
+mn.shape('min')
+hr.shape('hour')
+sc.st()
+mn.st()
+hr.st()
+wn.onclick(switch)
+wn.listen()
+s = input("Hello:")
 wn.mainloop()
